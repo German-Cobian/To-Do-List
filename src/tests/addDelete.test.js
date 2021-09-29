2de7fe4c69fe1bc17942aa94559848797001a4a5
Amalcxc <amalcxirsi@gmail.com>
2021-09-30 00:34:32 +0300

    add and remove

--- src/tests/addDelete.test.js
+++ src/tests/addDelete.test.js
@@ -33,3 +33,31 @@ describe('It adds and deletes items from the list', () => {
 
     expect(ul.innerHTML.includes(activityDescription)).toBe(true);
   });
+
+  test('it adds and deletes an item from the To Do List in the DOM', () => {
+    const addedActivity1 = inputActivity(activity1.description, activity1.completed, activity1.index);
+    const addedActivity2 = inputActivity(activity2.description, activity2.completed, activity2.index);
+    ul.innerHTML = '';
+    const addActivityToDOM = (activity) => {
+     const listElement = document.createElement('li');
+     listElement.classList.add('listItems');
+     listElement.setAttribute('activity', activity.index);
+     const input = document.createElement('input');
+     input.classList.add('completed');
+     input.setAttribute('checked', (activity.completed ? 'true' : 'false'));
+     const p = document.createElement('p');
+     p.classList.add('description');
+     p.textContent = activity.description;
+     listElement.appendChild(input);
+     listElement.appendChild(p);
+     ul.appendChild(listElement);
+    };
+    addActivityToDOM(addedActivity1);
+    addActivityToDOM(addedActivity2);
+    document.body.appendChild(ul);
+    ul.removeChild(ul.childNodes[0]);
+    repopulateList();
+    expect(activities.length).toBe(1);
+    expect(activities[0].description).toBe(addedActivity2.description);
+   });
+  });
