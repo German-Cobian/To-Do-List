635ecc99b4ff75d440d64b974b03ad33764818e0
German Renato Cobian <germancobian@hotmail.com>
2021-09-30 12:32:42 -0700

    Create test updates the status of checkbox: checked or unchecked

--- src/tests/statusContent.test.js
+++ src/tests/statusContent.test.js
@@ -1,3 +1,8 @@
+/* eslint-disable max-len */
+/**
+ * @jest-environment jsdom
+ */
+
 import {
   activities,
   inputActivity,
@@ -6,24 +11,40 @@ import {
   updateCheckboxStatus,
   editActivityDescription,
  } from './functionalitiesMock';
+
  import { activitiesList, clearCompleted } from '../indexAlt';
+
  describe('It updates status and content', () => {
   const activity1 = {
    description: 'Task 1',
    completed: true,
    index: 0,
   };
+
   const activity2 = {
    description: 'Task 2',
    completed: false,
    index: 1,
   };
+
   const storage = new LocalStorageMock();
   const ul = document.createElement('ul');
+
   test('It edits the task description', () => {
    const nextIndex = activities[activities.length];
    inputActivity('initial description', false, nextIndex);
    editActivityDescription(nextIndex, 'updated description');
+
    expect(activities[activities.length - 1].description).toBe('updated description');
   });
- });
+
+  test('Updates the status of checkbox: checked or unchecked', () => {
+    const nextIndex = activities[activities.length - 1];
+    inputActivity('Pending task', false, nextIndex);
+    updateCheckboxStatus(nextIndex, true);
+
+    expect(activities[activities.length - 1].completed).toBe(true);
+  });
+
+
+});
